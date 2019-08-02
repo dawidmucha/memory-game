09fa80070869b0e8bbcbb9cb70b5f63a72d49888
Dawid Mucha <dawidmucha98@gmail.com>
2019-08-02 19:37:15 +0200

    Updates

---
+++ precache-manifest.4c8456ee3b0523a086e674182ec8d9d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27ad2e16f9352f1e6b23ebc5b668216b",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "083b55e7daf54bf6de7b",
+    "url": "/memory-game/static/css/main.8a8e06cb.chunk.css"
+  },
+  {
+    "revision": "1733fca74707105c74e5",
+    "url": "/memory-game/static/js/2.37f69103.chunk.js"
+  },
+  {
+    "revision": "083b55e7daf54bf6de7b",
+    "url": "/memory-game/static/js/main.1735154c.chunk.js"
+  },
+  {
+    "revision": "799512f57ffe7b17c145",
+    "url": "/memory-game/static/js/runtime~main.4bfb86a8.js"
+  }
+]);
